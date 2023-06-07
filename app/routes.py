07ef412511c4432c88e362be1145459125255361
danonss0 <l.doniec@interia.pl>
2023-06-07 12:59:00 +0200

    2023-06-07 ŁD

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,20 @@
 from app import app
+from flask import render_template
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Łukasz Doniec!"
+    return render_template("index.html")
 
-@app.route('/name/', defaluts={'name: None'})
-@app.route('/name/<name>')
-def name(name):
-    return f"Hello, {name}!"
+@app.route('/extract')
+def extract():
+    return render_template("extract.html")
+
+@app.route('/products')
+def extract():
+    return render_template("products_list.html")
+
+@app.route('/product')
+def extract():
+    return render_template("product.html")
